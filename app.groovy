26251f0aac4b51e7df70c3e221840a06a54f4e19
shigemk2 <i.am.shige@gmail.com>
2016-06-04 18:07:19 +0900

    form

--- app.groovy
+++ app.groovy
@@ -2,11 +2,22 @@
 
 @Controller
 class MyBootApp {
-  @RequestMapping("/")
+  @RequestMapping(value="/",method=RequestMethod.GET)
   @ResponseBody
   def top(ModelAndView mv) {
     mv.setViewName("index")
     mv.addObject("title", "Hello!")
-    mv.addObject("msg", "welcome to Thymeleaf!!")
+    mv.addObject("msg", "please type any word....")
+    mv.addObject("val", "")
+  }
+
+  @RequestMapping(value="/",method=RequestMethod.POST)
+  @ResponseBody
+  def posted(@RequestParam("txt1")String txt1,
+             ModelAndView mv) {
+    mv.setViewName("index")
+    mv.addObject("title", "Hello!")
+    mv.addObject("msg", "you typed: " + txt1)
+    mv.addObject("val", txt1)
   }
 }
